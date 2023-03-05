bf68eea89f674e2ca1e48af958a690bc2cd32fd4
diango <yango_tascon@hotmail.com>
2023-03-05 01:51:49 +0100

    edit pass,edit user,registro

---
+++ src/components/likeVotes.js
@@ -0,0 +1,48 @@
+import { useContext, useState } from "react";
+import { sendLikeVotes } from "../services";
+import { AuthContext } from "../context/AuthContext";
+import "./likeVotes.css";
+
+
+export const LikeVotes = ({defaultValue, id}) => {
+    const [active, setActive] = useState(true);
+    const { token } = useContext(AuthContext);
+    const [vote, setVote] = useState(defaultValue);
+    const[dato, setDatos] =useState("");
+    const [error, setError] = useState("");
+  
+    const handlePlus = async (e) => {
+      e.preventDefault();
+      setError("");
+      try {
+       const data = await sendLikeVotes({ vote, token, id });
+       setVote( vote) 
+       setDatos(data)
+       setActive(!active)
+       setTimeout(()=>{
+        setDatos(null)
+      }, 5000)
+    
+    
+      } catch (error) {
+        setError(error.message);
+        setTimeout(()=>{
+          setError(null)
+        }, 5000)
+       
+      }
+    }; 
+    return (
+        <>
+
+        <span 
+      onClick={handlePlus}
+    >
+      {active ? "♡" : "♥" }
+    </span>
+        {vote ? <p className="datoVote">{dato}</p> : null}
+        {error ? <p className="errorVote">{error}</p> : null}
+        </>
+    );
+}
+
